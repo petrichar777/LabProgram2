222737bc0ec30a52e75bf8c3154c488886e98424
tinacaid <3103719905@qq.com>
2024-07-11 13:08:30 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -70,4 +70,13 @@ Route::post('user/viewInnovationStar',[\App\Http\Controllers\WkxController::clas
 Route::post('user/editInnovationStar',[\App\Http\Controllers\WkxController::class,'xiugai']);//修改
 
 Route::post('user/deleteInnovationStar',[\App\Http\Controllers\WkxController::class,'shanchu']);//删除
+Route::post('editScienceStar',[\App\Http\Controllers\MhwController::class,"editScienceStar"]);//科技之星学生信息修改
+Route::post('admin/login',[\App\Http\Controllers\MhwController::class,"adminlogin"]);//科技之星学生信息修改
+//Route::post('admin/logout',[\App\Http\Controllers\MhwController::class,"adminlogout"]);
+Route::post('user/editCompetitionStar',[\App\Http\Controllers\MhwController::class,"usereditCompetitionStar"]);
+Route::post('adminzhuce',[\App\Http\Controllers\MhwController::class,"zhuceadmin"]);
+Route::middleware('jwt.role:administrators')->prefix('administrators')->group(function () {
+    Route::post('logout',[\App\Http\Controllers\MhwController::class,'adminlogout']);//登出用户
 
+});
+Route::post('user/addInnovationStar',[\App\Http\Controllers\MhwController::class,"useraddInnovationStar"]);
