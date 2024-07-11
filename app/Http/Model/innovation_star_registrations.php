ecb3aad0c5ed6ca03c1fa807c52edba221bef355
tinacaid <3103719905@qq.com>
2024-07-11 13:06:33 +0800

    Update innovation_star_registrations.php

--- app/Http/Model/innovation_star_registrations.php
+++ app/Http/Model/innovation_star_registrations.php
@@ -77,4 +77,42 @@ class innovation_star_registrations extends Model
         } catch (Exception $e) {
             return 'error' . $e->getMessage();
         }
+         public  static function zengtian($data)
+    {
+        try {
+            $dat = DB::table('innovation_star_registrations')
+                ->insert([
+                    'grade' =>$data['grade'],
+                    'major'=>$data['major'],
+                    'class'=>$data['class'],
+                    'name' =>$data['name'],
+                    'entity_type'=>$data['entity_type'],//实体，虚体
+                    'company_name'=>$data['company_name'],//公司名称
+                    'registration_time'=>now(),//公司注册时间
+                    'certificate'=>$data['certificate'],//证书链接
+                    'applicant_ranking'=> 0,//申请人排名
+                    'company_scale'=> 0,
+                    'status'=> 0,//无关紧要的先设置为0
+                    'rejection_reason'=> 0,
+                    'student_id'=>$data['student_id'],
+                    'created_at'=>now(),
+                    'updated at'=>now(),
+                ]);
+            return $dat;
+        } catch (Exception $e) {
+            return 'error' . $e->getMessage();
+        }
+    }
+    public  static function shuliang($data)
+    {
+        try {
+            $tt=innovation_star_registrations::where('student_id', $data['student_id'])
+                ->select(['student_id'])
+                ->count();           // 明确指定列名
+                return $tt;
+        } catch (Exception $e) {
+
+            return 'error' . $e->getMessage();
+        }
+    }
 }
