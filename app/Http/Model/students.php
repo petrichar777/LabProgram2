2c7ef3977cb0dd486b77b09d3cecc8faba48c6c8
Peiling123 <147738440+Peiling123@users.noreply.github.com>
2024-07-11 12:27:54 +0800

    Update students.php

--- app/Http/Model/students.php
+++ app/Http/Model/students.php
@@ -24,6 +24,9 @@ class students extends Model
         'name',
         'email'
     ];
+    protected $hidden = [
+        'password',
+    ];
 
     // 设置与guestinfo的关联方法，方法名建议使用被关联表的名字
 
@@ -40,6 +43,45 @@ class students extends Model
 
         } catch (Exception $e) {
             return 'error ' . $e->getMessage();
+        }
+    }
+    public static function findstudent_id($user){
+        try {
+            $data = students::where('name',$user['name'])
+                ->where('grade',$user['grade'])
+                ->where('major',$user['major'])
+                ->where('class',$user['class'])
+                ->first();
+            return $data;
+        } catch (Exception $e) {
+            return 'error' . $e->getMessage();
+        }
+    }
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    // 验证凭证的静态方法
+    public static function validateCredentials($account, $password)
+    {
+        try {
+            // 查找用户
+            $user = self::where('account', $account)->first();
+
+            // 检查用户是否存在以及密码是否正确
+            if ($user && Hash::check($password, $user->password)) {
+                // 如果密码正确，返回 true
+                return true;
+            }
+
+            // 如果用户不存在或密码不正确，返回 false
+            return false;
+
+        } catch (Exception $e) {
+            // 处理异常，记录日志或返回错误信息
+            // Log::error('Error validating credentials: ' . $e->getMessage());
+            return false;
         }
     }
 }
